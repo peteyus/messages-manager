2729ba0985c198894ba8e292647f3c4e47ac09d5
Peter Jorgensen <prjorgen@gmail.com>
2024-03-06 12:38:49 -0700

    Tests work but lets get to testing in a way I have a tutorial...

--- MessagesManager/ClientApp/src/app/import/import.component.spec.ts
+++ MessagesManager/ClientApp/src/app/import/import.component.spec.ts
@@ -1,29 +1,29 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { ImportComponent } from './import.component';
-import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse, HttpRequest } from '@angular/common/http';
 import { InjectionToken } from '@angular/core';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatIconModule } from '@angular/material/icon';
+import { asyncData, asyncError } from '../testing/observable-testing-helper';
 
 describe('ImportComponent', () => {
   const BASE_URL = new InjectionToken<string>('base_url/');
 
   let component: ImportComponent;
   let fixture: ComponentFixture<ImportComponent>;
-  let httpClient: HttpClient;
-  let httpTestingController: HttpTestingController;
+  let httpClientSpy = jasmine.createSpyObj('HttpClient', ['post']);
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [HttpClientTestingModule],
+      imports: [HttpClientTestingModule, MatCheckboxModule, MatIconModule],
       declarations: [ImportComponent],
       providers: [
-        {provide: BASE_URL, useValue: 'base_url/'}
+        {provide: 'BASE_URL', useValue: BASE_URL},
+        {provide: HttpClient, useValue: httpClientSpy }
       ]
     })
     .compileComponents();
-
-    httpClient = TestBed.inject(HttpClient);
-    httpTestingController = TestBed.inject(HttpTestingController);
     
     fixture = TestBed.createComponent(ImportComponent);
     component = fixture.componentInstance;
@@ -42,9 +42,8 @@ describe('ImportComponent', () => {
         ]
       }
     }
-
+    httpClientSpy.post.and.returnValue(asyncData({}));
     component.onFileSelected(event);
-    const request = httpTestingController.expectOne('base_url/api/import/upload');
-    expect(request.request.method).toEqual('POST');
+    expect(httpClientSpy.post.calls.count()).toBe(1);
   })
 });
