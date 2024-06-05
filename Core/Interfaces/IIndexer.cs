ae48ff5f50325e193ad17ab3ba3767dfff97edb0
Peter Jorgensen <prjorgen@gmail.com>
2024-06-04 20:08:54 -0600

    Create indexer interface.

---
+++ Core/Interfaces/IIndexer.cs
@@ -0,0 +1,10 @@
+﻿using Core.Models;
+
+namespace Core.Interfaces
+{
+    public interface IIndexer
+    {
+        void IndexMessage(Message message);
+        IEnumerable<Message> SearchMessages(string searchString);
+    }
+}
