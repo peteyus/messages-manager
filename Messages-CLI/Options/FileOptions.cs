894efac278c91e0457131a9556fbace243c3d00f
Peter Jorgensen <prjorgen@gmail.com>
2024-06-11 22:55:18 -0600

    Add a multi file option

--- Messages-CLI/Options/FileOptions.cs
+++ Messages-CLI/Options/FileOptions.cs
@@ -1,5 +1,7 @@
 ﻿using System.CommandLine;
+using System.ComponentModel;
 using System.IO.Abstractions;
+using System.Runtime.CompilerServices;
 
 namespace Messages.CLI.Options
 {
@@ -10,7 +12,6 @@ namespace Messages.CLI.Options
             return new Option<IFileInfo?>(
             name: "--file",
             description: "The path to a file to interact with.",
-            isDefault: true,
             parseArgument: result =>
             {
                 if (result.Tokens.Count == 0)
@@ -19,7 +20,7 @@ namespace Messages.CLI.Options
                 string? filePath = result.Tokens.Single().Value;
                 if (!fileSystem.File.Exists(filePath))
                 {
-                    result.ErrorMessage = "The file does not exist.";
+                    result.ErrorMessage = $"The file {filePath} does not exist.";
                     return null;
                 }
                 else
@@ -27,6 +28,31 @@ namespace Messages.CLI.Options
                     return fileSystem.FileInfo.New(filePath);
                 }
             });
+        }
+
+        internal static Option<IEnumerable<IFileInfo>> BuildMultiFileOption(IFileSystem fileSystem)
+        {
+            return new Option<IEnumerable<IFileInfo>>(
+                aliases: new[] { "--file", "--files", "--filenames" },
+                description: "The path to one ore more files to interact with.",
+                parseArgument: result =>
+                {
+                    if (result.Tokens.Count == 0)
+                        throw new ArgumentException("One or more file names must be provided.");
+
+                    var files = new List<IFileInfo>();
+                    foreach (var token in result.Tokens)
+                    {
+                        string filePath = token.Value;
+                        if (!fileSystem.File.Exists(filePath))
+                        {
+                            throw new ArgumentException($"The file {filePath} does not exist.");
+                        }
+                        files.Add(fileSystem.FileInfo.New(filePath));
+                    }
+
+                    return files;
+                });
         }
     }
 }
